9b4363f5e443c658ce3c7d0ded95087bed6e087d
mkhayralla <gonfreecs303@gmail.com>
2017-11-22 23:06:03 +0200

    https

--- app.js
+++ app.js
@@ -48,7 +48,19 @@ app.use(cookieParser('12345-67890-09876-54321'));
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use('/', index);
 app.use('/users', users);
 
